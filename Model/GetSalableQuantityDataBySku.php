5f501d557a0b065e1919f1bb612733dec8d0856c
Nguyen Van Cuong <cuongnv@magenest.com>
2020-12-17 13:07:55 +0700

    PJB-025: Add reservation column to product grid

---
+++ Model/GetSalableQuantityDataBySku.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Copyright © 2020 Magenest. All rights reserved.
+ * See COPYING.txt for license details.
+ *
+ * Magenest_ReservationStockUi extension
+ * NOTICE OF LICENSE
+ *
+ * @category Magenest
+ * @package Magenest_ReservationStockUi
+ */
+
+namespace Magenest\ReservationStockUi\Model;
+
+use Magento\InventoryApi\Api\StockRepositoryInterface;
+use Magento\InventorySalesApi\Api\GetProductSalableQtyInterface;
+use Magento\InventoryReservationsApi\Model\GetReservationsQuantityInterface;
+use Magento\InventoryConfigurationApi\Api\GetStockItemConfigurationInterface;
+use Magento\InventorySalesAdminUi\Model\ResourceModel\GetAssignedStockIdsBySku;
+
+class GetSalableQuantityDataBySku extends \Magento\InventorySalesAdminUi\Model\GetSalableQuantityDataBySku
+{
+    /**
+     * @var GetProductSalableQtyInterface
+     */
+    private $getProductSalableQty;
+
+    /**
+     * @var StockRepositoryInterface
+     */
+    private $stockRepository;
+
+    /**
+     * @var GetAssignedStockIdsBySku
+     */
+    private $getAssignedStockIdsBySku;
+
+    /**
+     * @var GetStockItemConfigurationInterface
+     */
+    private $getStockItemConfiguration;
+
+    /**
+     * @var GetReservationsQuantityInterface
+     */
+    private $getReservationsQuantity;
+
+    /**
+     * GetSalableQuantityDataBySku constructor.
+     *
+     * @param GetReservationsQuantityInterface $getReservationsQuantity
+     * @param GetProductSalableQtyInterface $getProductSalableQty
+     * @param StockRepositoryInterface $stockRepository
+     * @param GetAssignedStockIdsBySku $getAssignedStockIdsBySku
+     * @param GetStockItemConfigurationInterface $getStockItemConfiguration
+     */
+    public function __construct(
+        GetReservationsQuantityInterface $getReservationsQuantity,
+        GetProductSalableQtyInterface $getProductSalableQty,
+        StockRepositoryInterface $stockRepository,
+        GetAssignedStockIdsBySku $getAssignedStockIdsBySku,
+        GetStockItemConfigurationInterface $getStockItemConfiguration
+    ) {
+        $this->getReservationsQuantity = $getReservationsQuantity;
+        $this->getProductSalableQty = $getProductSalableQty;
+        $this->stockRepository = $stockRepository;
+        $this->getAssignedStockIdsBySku = $getAssignedStockIdsBySku;
+        $this->getStockItemConfiguration = $getStockItemConfiguration;
+        parent::__construct($getProductSalableQty, $stockRepository, $getAssignedStockIdsBySku, $getStockItemConfiguration);
+    }
+
+    /**
+     * @param string $sku
+     *
+     * @return array
+     * @throws \Magento\Framework\Exception\InputException
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws \Magento\InventoryConfigurationApi\Exception\SkuIsNotAssignedToStockException
+     */
+    public function execute(string $sku): array
+    {
+        $stockInfo = [];
+        $stockIds = $this->getAssignedStockIdsBySku->execute($sku);
+        if (count($stockIds)) {
+            foreach ($stockIds as $stockId) {
+                $stockId = (int)$stockId;
+                $stock = $this->stockRepository->get($stockId);
+                $stockItemConfiguration = $this->getStockItemConfiguration->execute($sku, $stockId);
+                $isManageStock = $stockItemConfiguration->isManageStock();
+                $stockInfo[] = [
+                    'stock_name' => $stock->getName(),
+                    'qty' => $isManageStock ? $this->getProductSalableQty->execute($sku, $stockId) : null,
+                    'manage_stock' => $isManageStock,
+                    'reservation_name' => __('Reservation Stock'),
+                    'reservation_qty' => $isManageStock ? $this->getReservationsQuantity->execute($sku, $stockId) : null
+                ];
+            }
+        }
+
+        return $stockInfo;
+    }
+}
