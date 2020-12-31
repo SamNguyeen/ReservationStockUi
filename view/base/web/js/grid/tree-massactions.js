c402c7f13c515e073f39f83aece5da95d2be16e5
Nguyen Van Cuong <cuongnv@magenest.com>
2020-12-31 15:36:24 +0700

    Add popup confirm before delete reservation

--- view/base/web/js/grid/tree-massactions.js
+++ view/base/web/js/grid/tree-massactions.js
@@ -10,10 +10,13 @@
  */
 define([
     'jquery',
-    'Magento_Ui/js/modal/confirm',
+    'mage/template',
+    'text!Magenest_ReservationStockUi/template/popup/confirm.html',
     'Magento_Ui/js/modal/alert',
-    'Magento_Ui/js/grid/tree-massactions'
-], function ($, confirm, alert, Massactions) {
+    'Magento_Ui/js/modal/modal',
+    'Magento_Ui/js/grid/tree-massactions',
+    'mage/translate'
+], function ($, template, confirmTpl, alert, modal, Massactions, $t) {
     'use strict';
 
     return Massactions.extend({
@@ -62,18 +65,53 @@ define([
          *      invoked if action is confirmed.
          */
         _confirm: function (action, callback) {
-            var confirmData = action.confirm,
+            let confirmData = action.confirm,
                 data = this.getSelections(),
                 total = data.total ? data.total : 0,
                 confirmMessage = confirmData.message + ' (' + total + ' record' + (total > 1 ? 's' : '') + ')';
-
-            confirm({
+            this.confirmModal = $('<div/>').modal({
                 title: confirmData.title,
-                content: confirmMessage,
-                actions: {
-                    confirm: callback
-                }
+                modalClass: 'reservation-confirmation-modal',
+                buttons: [
+                    {
+                        text: $.mage.__('Cancel'),
+                        class: 'action-secondary action-dismiss',
+                        attr: {},
+                        click: function (event) {
+                            $('#confirm-error-message').attr('style', 'display:none');
+                            this.closeModal(event);
+                        }
+                    },
+                    {
+                        text: $.mage.__('Confirm Delete'),
+                        class: 'action-primary action-accept',
+                        attr: {},
+                        click: function () {
+                            $('#confirm-error-message').attr('style', 'display:none');
+                            let confirmText = $('#delete-reservation-confirm').val().toLowerCase();
+                            if (confirmText == 'confirm') {
+                                callback();
+                            } else {
+                                $('#confirm-error-message').attr('style', 'display:block');
+                            }
+                        }
+                    }
+                ],
             });
+            this.confirmModal.modal('openModal').append(this._getContent(confirmMessage));
+
+            return false;
+        },
+
+        _getContent: function (confirmMessage) {
+            var confirmPopup = template(confirmTpl, {
+                confirmMessage: confirmMessage,
+                noticeMsg: $.mage.__('Once you confirm and press <strong>Confirm Delete</strong>, it cannot be undone or recovered.'),
+                confirmMsg: $.mage.__('To confirm, please type: <em>confirm</em>'),
+                errorMsg: $.mage.__('Please type confirm in this field.')
+            });
+
+            return confirmPopup;
         }
     });
 });
